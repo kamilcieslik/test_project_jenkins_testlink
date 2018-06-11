f2adc6238f7eba3e863a755a7839b1f08b334f5c
Kamil Cieślik <mrfarinq@hotmail.com>
2018-06-11 14:14:46 +0200

    Update run_tests.sh

--- run_tests.sh
+++ run_tests.sh
@@ -3,7 +3,6 @@ set -e
 virtualenv venv --distribute -p python3
 source venv/bin/activate
 pip install -r requirements.txt
-PYTHONPATH=. venv/bin/python3 tests/calculations_tests.py
 PYTHONPATH=. venv/bin/coverage run tests/calculations_tests.py
 PYTHONPATH=. venv/bin/coverage report --omit=venv/*
 PYTHONPATH=. venv/bin/coverage html --omit=venv/*
